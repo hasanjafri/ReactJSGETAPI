7d7738c31b262ffa02d66625c57caddb73016173
Jafri, Hasan <hasan.jafri@nuance.com>
2017-12-14 19:12:35 -0500

    WIP: POST Request to moccast-server

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -24,6 +24,22 @@ class Recorder extends Component {
   
   onStop(recordedBlob) {
     console.log('recordedBlob is: ', recordedBlob);
+
+    var reader = new window.FileReader();
+    reader.readAsDataURL(recordedBlob.blob);
+    reader.onloadend = function() {
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://localhost:5000/vp', true);
+      xhr.onload = function () {
+          // do something to response
+          console.log(this.responseText);
+      };
+      var formData = new FormData()
+      formData.append("household_id", 3);
+      formData.append("audio", recordedBlob.blob)
+      xhr.send(formData)
+      //xhr.send('household_id=3&audio='+reader.result); 
+    }
   }
 
   render() {
@@ -36,8 +52,7 @@ class Recorder extends Component {
           strokeColor="#000000"
           width={160}
           height={25}
-          audioBitsPerSecond={8000}
-          mimeType="audio/wav"/>
+          audioBitsPerSecond={8000}/>
         <button onTouchTap={this.startRecording} type="button">Start</button>
         <button onTouchTap={this.stopRecording} type="button">Stop</button>
       </div>
